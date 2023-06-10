f4e0fd9b24960e6e07ee3033f7cbb596a7da9d44
jonty472 <azttorrs@gmail.com>
2023-06-11 11:41:12 +1200

    Add project files.

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TemperatureConverterTool;
+
+Console.Write("Conversions -\n1. To fahrenheit\n2. To celsius\n: ");
+string ConversionType = Console.ReadLine();
+int ConverstionType = Convert.ToInt16(ConversionType);
+
+Console.Write("Enter temperature: ");
+string Temperature = Console.ReadLine();
+float TemperatureFloat = Convert.ToSingle(Temperature);
+
+TemperatureConverter conversion = new TemperatureConverter(ConverstionType, TemperatureFloat);
+if (conversion.ConversionType == 2)
+{
+    Console.WriteLine(conversion.ToCelsius(conversion.TemperatureValue));
+}
+
---
+++ TemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverterTool
+{
+    internal class TemperatureConverter
+    {
+        public int ConversionType { get; set; }    
+        public float TemperatureValue { get; set; }
+
+        public TemperatureConverter (int ConversionType, float TemperatureValue)
+        {
+            this.TemperatureValue = TemperatureValue;
+            this.ConversionType = ConversionType;
+        }
+
+        public double ToCelsius(float TemperatureValue)
+        {
+            var TemperatureC = (5/9.0) * (TemperatureValue - 32);
+
+            return Math.Round(TemperatureC, 2);
+        }
+
+        public void ToFahrenheit(float TemperatureValue)
+        {
+
+        }
+
+
+
+    }
+}
